21dee8e364e15b148d737a409277a57646ce0adc
Jon Lau <jonlau@Jons-MacBook-Air.local>
2018-01-22 17:34:25 -0800

    inputs returning from server and rendering on page

--- client/src/app.jsx
+++ client/src/app.jsx
@@ -1,20 +1,65 @@
-import React from ('react');
-import ReactDOM from ('react-dom');
-import $ from ('jquery');
+import React from 'react';
+import ReactDOM from 'react-dom';
+import $ from 'jquery';
 
 
-class App extends React.component {
+class App extends React.Component {
   constructor(props) {
-    this.state = {input: ''}
+    super(props);
+    this.state = {
+      input: '',
+      sorted: ''
+    }
+    
+    this.updateTerm = this.updateTerm.bind(this);
+    this.submit = this.submit.bind(this);
   }
   
+  updateTerm(event) {
+    this.setState({
+      input: event.target.value
+    })
+    console.log(this.state.input);
+  }
+  
+  submit() {
+    let searchTerm = this.state.input;
+
+    fetch('/sort', {
+      method: 'POST',
+      body: JSON.stringify({'searchTerm': searchTerm}),
+      headers: {
+        'Content-Type': 'application/json',
+        'Accept': 'application/json'
+      }
+    })
+    .then((res) => {
+      return res.json();
+    })
+    .catch((error) => {
+      console.log(error);
+    })
+    .then((response) => {
+      console.log(response.message);
+      this.setState({
+        sorted: response.message
+      })
+    })
+    // .catch((error) => {
+    //   console.log('Error:', error)
+    // })
+  }
+
   render() {
     return(
       <div>
-        <input type='text'>
-        </div>
+        <input type='text' onChange={this.updateTerm}></input>
+        <button onClick={this.submit}>Submit</button>
+        <p>{this.state.sorted}</p>
+      </div>
     )
   }
+
 }
 
 ReactDOM.render(<App/>, document.getElementById('app'));
--- server/index.js
+++ server/index.js
@@ -6,8 +6,14 @@ const app = express();
 app.use(bodyParser.json());
 app.use(express.static(__dirname + './../client/dist'));
 
-app.get('/', (req, res) => {
+app.get('/test', (req, res) => {
+  console.log(req.body);
   res.status(200).send('mesage recieved');
+})
+
+app.post('/sort', (req, res) => {
+  console.log(req.body.searchTerm);
+  res.status(201).send({'message': req.body.searchTerm});
 })
 
 app.listen(3000, () => {
